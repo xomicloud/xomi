1bf4cccd69a1bea77df802a5d2306f6af5fa5020
James Smith <james.smith@djalbat.com>
2021-07-01 20:07:11 +0100

    Added more to the authorisation utilities.

--- bin/utilities/authorisation.js
+++ bin/utilities/authorisation.js
@@ -1,6 +1,10 @@
 "use strict";
 
+const { arrayUtilities } = require("necessary");
+
 const { BASE_64 } = require("../constants");
+
+const { second, third } = arrayUtilities;
 
 function createBasicAuthorisation(options) {
   const { clientId, clientSecret } = options,
@@ -11,6 +15,53 @@ function createBasicAuthorisation(options) {
   return basicAuthorisation;
 }
 
+function clientIdFromBasicAuthorisation(basicAuthorisation) {
+  let clientId = null;
+
+  const digest = digestFromBasicAuthorisation(basicAuthorisation),
+        matches = digest.match(/^([^:]+):([^:]+)$/);
+
+  if (matches !== null) {
+    const secondMatch = second(matches);
+
+    clientId = secondMatch;  ///
+  }
+
+  return clientId;
+}
+
+function clientSecretFromBasicAuthorisation(basicAuthorisation) {
+  let clientSecret = null;
+
+  const digest = digestFromBasicAuthorisation(basicAuthorisation),
+        matches = digest.match(/^([^:]+):([^:]+)$/);
+
+  if (matches !== null) {
+    const thirdMatch = third(matches);
+
+    clientSecret = thirdMatch;  ///
+  }
+
+  return clientSecret;
+}
+
 module.exports = {
-  createBasicAuthorisation
+  createBasicAuthorisation,
+  clientIdFromBasicAuthorisation,
+  clientSecretFromBasicAuthorisation
 };
+
+function digestFromBasicAuthorisation(basicAuthorisation) {
+  let digest = null;
+
+  const matches = basicAuthorisation.match(/^Basic (.+)$/i);
+
+  if (matches !== null) {
+    const secondMatch = second(matches),
+          encodedDigest = secondMatch;  ///
+
+    digest = Buffer.from(encodedDigest, BASE_64).toString();
+  }
+
+  return digest;
+}

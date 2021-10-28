a4e376cd1690ce78b117bb45e950be8cd2473db3
James Smith <james.smith@djalbat.com>
2021-10-28 13:09:55 +0100

    Removed the callback functionality from the api() function.

--- bin/api.js
+++ bin/api.js
@@ -2,8 +2,7 @@
 
 const { requestUtilities } = require("necessary");
 
-const { FUNCTION } = require("./constants"),
-      { pipeline } = require("./utilities/response"),
+const { pipeline } = require("./utilities/response"),
       { badGatewayError } = require("./http"),
       { DEFAULT_API_HOST } = require("./defaults"),
       { createBasicAuthorisation } = require("./utilities/authorisation"),
@@ -13,10 +12,6 @@ const { FUNCTION } = require("./constants"),
 const { request: makeRequest } = requestUtilities;
 
 function api(options, request, response) {
-  if (typeof request === FUNCTION) {
-    request = request();  ///
-  }
-
   const { url, query, method } = request,
         { apiHost = DEFAULT_API_HOST } = options,
         basicAuthorisation = createBasicAuthorisation(options),

4d63fef7509979127a2beaac93e00e9d72fbaca1
James Smith <james.smith@djalbat.com>
2022-03-10 14:22:05 +0000

    A slight improvement to the api() function.

--- bin/api.js
+++ bin/api.js
@@ -4,19 +4,17 @@ const { requestUtilities } = require("necessary");
 
 const httpResponse = require("./httpResponse");
 
-const { uriFromURL } = require("./utilities/request"),
-      { DEFAULT_API_HOST } = require("./defaults"),
+const { DEFAULT_API_HOST } = require("./defaults"),
       { setStatus, setHeaders } = require("./utilities/response"),
       { createBasicAuthorisation } = require("./utilities/authorisation");
 
 const { createRequest: createRemoteRequest } = requestUtilities;
 
 function api(configuration, request, response) {
-  const { url, query, method, headers } = request,
-        { apiHost = DEFAULT_API_HOST } = configuration,
+  const { apiHost = DEFAULT_API_HOST } = configuration,
+        { uri, query, method, headers } = request,
         basicAuthorisation = createBasicAuthorisation(configuration),
         authorization = basicAuthorisation,  ///
-        uri = uriFromURL(url),
         host = apiHost;  ///
 
   Object.assign(headers, {
